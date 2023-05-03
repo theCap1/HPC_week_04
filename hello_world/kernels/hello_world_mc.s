b5c145578bb68d294c2ac3412a23cee66344bfc6
theCap1 <sbp.programming@gmail.com>
2023-05-03 21:48:54 +0000

    majority of 5.2 done

---
+++ hello_world/kernels/hello_world_mc.s
@@ -0,0 +1,23 @@
+        .section    .rodata             // read only data section
+my_msg: .asciz      "Hello World!\n"    // null-terminated string
+
+        .text                           // text section
+        .global hello_world_main
+
+        /*
+         * prints "Hello World!\n" and returns 0.
+         */
+hello_world_main:   stp     x29, x30, [sp, #-16]!
+
+        // print "Hello World!\n" by calling printf
+        adr     x0, my_msg
+        bl      printf
+
+        // return 0
+        .word   0x1A8003E0          // orr w0, w0, wzr
+        ldp     x29, x30, [sp], #16
+
+        ret
+
+        // size of main section
+        .size   hello_world_main, (. - hello_world_main)
